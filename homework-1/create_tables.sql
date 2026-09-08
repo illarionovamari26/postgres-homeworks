331868d80213c31d9bf797b445678c7e8e59eb72
Мария <maria@MacBook-Air-Maria.local>
2026-09-08 17:05:17 +0300

    Решение домашнего задания 1

---
+++ homework-1/create_tables.sql
@@ -0,0 +1,26 @@
+-- SQL-команды для создания таблиц
+-- Таблица сотрудников
+CREATE TABLE employees (
+    employee_id int PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    title varchar(100) NOT NULL,
+    birth_date date,
+    notes text
+);
+
+-- Таблица клиентов
+CREATE TABLE customers (
+    customer_id varchar(10) PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+    contact_name varchar(100) NOT NULL
+);
+
+-- Таблица заказов
+CREATE TABLE orders (
+    order_id int PRIMARY KEY,
+    customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+    employee_id int REFERENCES employees(employee_id) NOT NULL,
+    order_date date NOT NULL,
+    ship_city varchar(100) NOT NULL
+);
---
+++ homework-1/main.py
@@ -0,0 +1,50 @@
+"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="maria",
+    password="",
+    port=5432,
+)
+cur = conn.cursor()
+
+# 1. Заполняем таблицу сотрудников (employees)
+with open('homework-1/north_data/employees_data.csv', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    next(reader)  # пропускаем строку с заголовками
+    for row in reader:
+        cur.execute(
+            "INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+            row
+        )
+
+# 2. Заполняем таблицу клиентов (customers)
+with open('homework-1/north_data/customers_data.csv', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    next(reader)  # пропускаем строку с заголовками
+    for row in reader:
+        cur.execute(
+            "INSERT INTO customers VALUES (%s, %s, %s)",
+            row
+        )
+
+# 3. Заполняем таблицу заказов (orders)
+with open('homework-1/north_data/orders_data.csv', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    next(reader)  # пропускаем строку с заголовками
+    for row in reader:
+        cur.execute(
+            "INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+            row
+        )
+
+# Сохраняем изменения и закрываем соединение
+conn.commit()
+cur.close()
+conn.close()
+
+print("Данные успешно добавлены!")
